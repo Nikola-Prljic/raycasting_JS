5b56da6c3ddb5409d076502ee72d6ded4e7eb86f
Niki42 <nik.7377@gmail.com>
2023-08-22 19:47:09 +0200

    hi

--- raycast.js
+++ raycast.js
@@ -3,7 +3,7 @@ const MAP_NUM_ROWS = 11;
 const MAP_NUM_CLOS = 15;
 
 const WINDOW_WIDTH = MAP_NUM_CLOS * TILE_SIZE;
-const WINDOWS_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
 class Map {
     constructor() {
@@ -35,18 +35,72 @@ class Map {
     }
 }
 
+class Player{
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        this.radius = 3;
+        this.turnDirection = 0; // -1 if left +1 if right
+        this.walkDirection = 0; // -1 if back +1 if front
+        this.rotationAngle = Math.PI / 2;
+        this.moveSpeed = 2.0;
+        this.rotationSpeed = 2 * (Math.PI / 180);
+    }
+    update() {
+        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+        
+        var moveStep = this.walkDirection * this.moveSpeed;
+        this.x += 
+    }
+    render() {
+        noStroke();
+        fill("red");
+        circle(this.x, this.y, this.radius);
+        stroke("red");
+        line(this.x, this.y, 
+            this.x + Math.cos(this.rotationAngle) * 30,
+            this.y + Math.sin(this.rotationAngle) * 30);
+    }
+}
+
 var grid = new Map();
+var player = new Player();
+
+function keyPressed() {
+    if (keyCode == UP_ARROW){
+        player.walkDirection = +1;
+    } else if (keyCode == DOWN_ARROW){
+        player.walkDirection = -1;
+    } else if (keyCode == RIGHT_ARROW){
+        player.turnDirection = +1;
+    } else if (keyCode == LEFT_ARROW){
+        player.turnDirection = -1;
+    }
+}
+
+function keyReleased() {
+    if (keyCode == UP_ARROW){
+        player.walkDirection = 0;
+    } else if (keyCode == DOWN_ARROW){
+        player.walkDirection = 0;
+    } else if (keyCode == RIGHT_ARROW){
+        player.turnDirection = 0;
+    } else if (keyCode == LEFT_ARROW){
+        player.turnDirection = 0;
+    }
+}
 
 function setup() {
-    createCanvas(WINDOW_WIDTH, WINDOWS_HEIGHT);
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update() {
+    player.update();
     // update game obj before next frame
 } 
 
 function draw() {
     update();
     grid.render();
-    // render
+    player.render();
 }
